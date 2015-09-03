7ac6165744e5819ea7f217ef381529270263917b
Mark Crossfield <github@nanunanu.co.uk>
2015-09-03 01:05:32 +0100

    Adds layer colours. Starts to migrate CDATA from KML files which was stripped.

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,49 +1,54 @@
 ---
 feature_layers:
-  - id: coal
-    title: Coal
-    file: map-data/coal.geojson
-  - id: coldWar
-    title: Cold War
-    file: map-data/cold-war.geojson
-  - id: miscellaneous
-    title: Miscellaneous
-    file: map-data/miscellaneous.geojson
-  - id: shelters
-    title: Shelters
-    file: map-data/shelters.geojson
   - id: transport
     title: Transport
     file: map-data/transport.geojson
-  - id: tunnels
-    title: Tunnels
-    file: map-data/tunnels.geojson
-  - id: victoriaStationArea
-    title: Victoria Station Area
-    file: map-data/victoria-station-area.geojson
+    layer-color: '#ff0000'
   - id: waterways
     title: Waterways
     file: map-data/waterways.geojson
+    layer-color: '#3887be'
+  - id: victoriaStationArea
+    title: Victoria & Cathedral
+    file: map-data/victoria-station-area.geojson
+    layer-color: '#ff9999'
+  - id: tunnels
+    title: Tunnels
+    file: map-data/tunnels.geojson
+    layer-color: '#5c2b00'
+  - id: coal
+    title: Coal
+    file: map-data/coal.geojson
+    layer-color: '#000000'
+  - id: miscellaneous
+    title: Miscellaneous
+    file: map-data/miscellaneous.geojson
+    layer-color: '#a8a8a8'
+  - id: shelters
+    title: Shelters
+    file: map-data/shelters.geojson
+    layer-color: '#00ff00'
 ---
 
 // Provide your access token
 L.mapbox.accessToken = 'pk.eyJ1IjoibWFya2Nyb3NzZmllbGQiLCJhIjoiYjJjNzliNGEwNjNiYTU1YjA4YTlkNjhkNmNmMjJlYzgifQ.2jm00t_mEEW5wEk6Ytzp2g';
 // Create a map in the div #map
-var map = L.mapbox.map('undergroundManchesterMap', 'markcrossfield.nae9omlm').setView([53.4728584,-2.2444749],13);
+var map = L.mapbox.map('undergroundManchesterMap', 'markcrossfield.nae9omlm', { zoomControl: false }).setView([53.4780584,-2.2414749],14);
+
+new L.Control.Zoom({ position: 'topright' }).addTo(map);
 map.scrollWheelZoom.disable();
 
 var layers = document.getElementById('menu-ui');
-addLayer(L.mapbox.tileLayer('examples.bike-lanes'), 'Bike Lanes', 2);
-{% for feature_layer in page.feature_layers %}
-    var {{feature_layer.id}} = {% include {{feature_layer.file}} %}
-    addLayer(L.mapbox.featureLayer({{feature_layer.id}}), "{{feature_layer.title}}", 2);
-{% endfor %}
 {% for feature in site.features %}
     var {{feature.layer}} = {{ feature.content }}
-    addLayer(L.mapbox.featureLayer({{feature.layer}}), "{{feature.layer-title}}", 2);
+    addLayer(L.mapbox.featureLayer({{feature.layer}}), "{{feature.layer-title}}", 2, "{{feature.layer-color}}");
+{% endfor %}
+{% for feature_layer in page.feature_layers %}
+    var {{feature_layer.id}} = {% include {{feature_layer.file}} %}
+    addLayer(L.mapbox.featureLayer({{feature_layer.id}}), "{{feature_layer.title}}", 2, "{{feature_layer.layer-color}}");
 {% endfor %}
 
-function addLayer(layer, name, zIndex) {
+function addLayer(layer, name, zIndex, color) {
     layer
         .setZIndex(zIndex)
         .addTo(map);
@@ -52,8 +57,10 @@ function addLayer(layer, name, zIndex) {
     // toggles layers on and off.
     var link = document.createElement('a');
     link.href = '#';
+    link.id = name
     link.className = 'active';
     link.innerHTML = name;
+    link.style.cssText = 'background-color: ' + color + ';';
 
     link.onclick = function(e) {
         e.preventDefault();
