66d129abf4271af4e17736f45ce7490f0d225279
Mark Crossfield <github@nanunanu.co.uk>
2016-04-17 14:44:21 +0100

    Switching to economy mode in case of incoming M.E.N. traffic.

--- javascripts/map.js
+++ javascripts/map.js
@@ -2,7 +2,7 @@
 ---
 // Provide your access token
 L.mapbox.accessToken = 'pk.eyJ1IjoibWFya2Nyb3NzZmllbGQiLCJhIjoiYjJjNzliNGEwNjNiYTU1YjA4YTlkNjhkNmNmMjJlYzgifQ.2jm00t_mEEW5wEk6Ytzp2g';
-var economy = false;
+var economy = {{site.economy}};
 var baseMap = economy? 'mapbox.satellite' : 'markcrossfield.nae9omlm';
 var map = L.mapbox.map('underground-manchester-map', baseMap, { zoomControl: false }).setView([53.4780584,-2.2414749],14);
 var layers = {
